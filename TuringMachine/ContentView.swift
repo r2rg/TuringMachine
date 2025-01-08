4f138899f33d9b69a49d9305871b1cf9d07e7b58
Arthur <arturik2004@yandex.ru>
2025-01-09 01:28:42 +0300

    Initial Commit

---
+++ TuringMachine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TuringMachine
+//
+//  Created by Артур Галустян on 09.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TuringMachine/TuringMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuringMachineApp.swift
+//  TuringMachine
+//
+//  Created by Артур Галустян on 09.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TuringMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
